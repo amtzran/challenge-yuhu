a84519a82660342d314db22ec33aa66b85e5b97a
Alberto Martinez <amtzran@gmail.com>
2023-10-26 17:12:15 -0600

    Add permissions for api tasks

--- app/users/permissions.py
+++ app/users/permissions.py
@@ -3,8 +3,8 @@ from rest_framework.permissions import BasePermission
 
 class AdminPanel(BasePermission):
     """
-    Allows access only to authenticated users that are employees.
+    Allows access only to authenticated users that are users.
     """
 
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated) and hasattr(request.user, "employee")
+        return bool(request.user and request.user.is_authenticated)
