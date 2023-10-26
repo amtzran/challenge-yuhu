7dfdb7ec41407995178a739292a0fe263f1effc2
Alberto Martinez <amtzran@gmail.com>
2023-10-26 15:14:32 -0600

    Fix router urls

--- app/api/urls.py
+++ app/api/urls.py
@@ -2,13 +2,10 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from catalogues.urls import ADMIN_CATALOGUES_ROUTER
-
 from .serializers import UserToken
 
 API_V1_ROUTER_ADMIN = DefaultRouter()
 
-API_V1_ROUTER_ADMIN.registry.extend(ADMIN_CATALOGUES_ROUTER.registry)
 # API_V1_ROUTER_ADMIN.registry.extend(ADMIN_USERS_ROUTER.registry)
 
 urlpatterns = [
