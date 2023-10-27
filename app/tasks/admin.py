6ed388f18763ccf4b54e4819ca8df27836545f66
Alberto Martinez <amtzran@gmail.com>
2023-10-26 22:49:58 -0600

    Add django admin for tasks

--- app/tasks/admin.py
+++ app/tasks/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        "random_slug",
+        "title",
+        "description",
+        "due_date",
+        "author",
+        "created_at",
+    ]
+    list_filter = ["due_date", "created_at"]
+    search_fields = ["title"]
