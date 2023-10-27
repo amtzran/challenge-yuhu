48e2e60ca671e5f54dfedb4f43748a2161d0991e
Alberto Martinez <amtzran@gmail.com>
2023-10-26 22:08:41 -0600

    Add celery

--- etc/scripts/run.sh
+++ etc/scripts/run.sh
@@ -6,3 +6,5 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module core.wsgi
+
+celery -A core worker --loglevel=info --concurrency 1 -E
